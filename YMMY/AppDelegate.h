7975af090ddcd5119b541c6a7278bd0c44efb3b8
apple <apple@macbook-pro.local>
2017-03-11 19:56:38 +0800

    Initial Commit

---
+++ YMMY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YMMY
+//
+//  Created by apple on 2017/3/11.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
